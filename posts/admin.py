38900da286a262f104e507423e09d8d5ef4bb890
ryanhong <eliminator1019@naver.com>
2021-02-04 16:28:42 +0900

    Posts Models and admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
+from . import models
 
 # Register your models here.
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    """Custom Post Admin """
+
+    list_display = (
+        "title",
+        "pub_date",
+    )
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition """
+
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Photo(models.Model):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField(upload_to="post_photos")
+    post = models.ForeignKey("Post", related_name="photos", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
+class Post(models.Model):
+    "Post Model Definition"
+
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=timezone.now)
+    video_url = models.URLField(blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
