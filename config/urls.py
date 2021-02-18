2b5f9916ad0b2aa839917acd2e125caf6d6e2a9b
ryanhong <eliminator1019@naver.com>
2021-02-18 19:37:25 +0900

    core-aboutandhomeconnect

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from votes import views as votes_view
 
 urlpatterns = [
+    path("", include("core.urls", namespace="core")),
     path('admin/', admin.site.urls),
-]
+   ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def homeview(request):
+    return render(request, "index.html")
+
+def aboutview(request):
+    return render(request, "about.html")
