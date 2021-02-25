616394d660385e0791efa3a85e1029247dddbd04
ryanhong <eliminator1019@naver.com>
2021-02-25 16:01:48 +0900

    fix votes model

--- votes/models.py
+++ votes/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200, verbose_name='질문 내용')
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='질문 생성 시각')
-
+    pub_date = models.DateTimeField(auto_now_add= True, verbose_name='질문 생성 시각')
+    description = models.TextField(verbose_name='설명', null=True)
+    
     def __str__(self):
         return self.question_text
 
