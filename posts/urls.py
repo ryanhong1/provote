9edfc67e55439d395bc74a0977abeb030c20c731
ryanhong <eliminator1019@naver.com>
2021-02-25 16:10:06 +0900

    post detail view

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "posts"
+
+urlpatterns = [
+    path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+   ]   
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . import models
+from django.views.generic import DetailView
 # Create your views here.
+
+class PostDetailView(DetailView):
+    model = models.Post
